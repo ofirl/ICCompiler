6ca34d39f461b16af2d7c63dd16597a1cd0e7882
Naor <Naor@Naor-PC.Home>
2014-11-07 12:11:27 +0200

    Added the classes Token and sym that were shown in the recitation.

---
+++ iCCompiler/Token.java
@@ -0,0 +1,18 @@
+package iCCompiler;
+
+import java_cup.runtime.Symbol;
+
+public class Token extends Symbol {
+
+	public int getId() {
+		return 0;
+	}
+
+	public Object getValue() {
+		return null;
+	}
+
+	public int getLine() {
+		return 0;
+	}
+}
---
+++ iCCompiler/sym.java
@@ -0,0 +1,6 @@
+package iCCompiler;
+
+public class sym {
+	public static final int EOF = 0;
+	public static final int ID = 1;
+}
