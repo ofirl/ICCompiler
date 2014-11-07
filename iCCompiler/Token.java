900e9e2f52acb6d2798d4f789a5127adc494beeb
Naor <Naor@10.0.0.7>
2014-11-07 19:38:26 +0200

    Added id field

--- iCCompiler/Token.java
+++ iCCompiler/Token.java
@@ -3,33 +3,34 @@ package iCCompiler;
 import java_cup.runtime.Symbol;
 
 public class Token extends Symbol {
-	
+
+	private int id;
 	private int line;
 	private int column;
 	private String tag;
 	private String value;
-	
-	public Token(int sym_num) { //not used, just so there wont be errors
+
+	public Token(int sym_num) { // not used, just so there wont be errors
 		super(sym_num);
 		// TODO Auto-generated constructor stub
 	}
 
 	public int getId() {
-		return 0;
+		return id;
 	}
 
 	public int getLine() {
 		return line;
 	}
-	
+
 	public int getColumn() {
 		return column;
 	}
-	
+
 	public String getTag() {
 		return tag;
 	}
-	
+
 	public String getValue() {
 		return value;
 	}
