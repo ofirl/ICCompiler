e6a89d5559aeea3c4e68e7c94543a166285b5ecd
ofirl <ofirl24@gmail.com>
2014-11-15 19:08:14 +0200

    add a functions that accepts string and returns a collection of tokens, (as stated in the assignment pdf).

--- iCCompiler/Main.java
+++ iCCompiler/Main.java
@@ -1,28 +1,44 @@
 package iCCompiler;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import JFlex.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-	    Token currToken;
+	    
+		Collection<Token> tokens = LexFile(args[0]);
+		
+		for (Token currToken : tokens){
+			PrintToken(currToken.getValue(), currToken.getTag(), currToken.getLine(), currToken.getColumn());
+		}
+	  }
+
+	public static Collection<Token> LexFile(String file)
+	{
+		Collection<Token> tokens = new ArrayList<>();
+		
+		Token currToken;
 	    try {
-	        FileReader txtFile = new FileReader(args[0]);
+	        FileReader txtFile = new FileReader(file);
 	        Lexer scanner = new Lexer(txtFile); 
 	        System.out.println("token\ttag\tline : column");
 	        
 	        do {
 	            currToken = scanner.next_token();
-	            PrintToken(currToken.getValue(), currToken.getTag(), currToken.getLine(), currToken.getColumn());
-	            
+	            tokens.add(currToken);	            
 	        } while (currToken.sym != sym.EOF);
 	    
 	    } catch (Exception e) {
 	        throw new RuntimeException("IO Error (brutal exit)" + e.toString());
 	    }
-	  }
-
+		
+		return tokens;
+	}
+	
 	/* This function was given in the exercise. */
 	public static void PrintToken(String token,String tag , int line , int column) {
 		System.out. println (token+"\t"+tag+"\t"+line+":"+column);
