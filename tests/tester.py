c6d024a2ab228759adfac502b61364f28dbb3d93
Maor <maor.dn@gmail.com>
2014-11-29 17:24:10 +0200

    added tests for classes in program

---
+++ tests/tester.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import os
+import sys
+import subprocess
+
+os.chdir(os.path.dirname(sys.argv[0]))
+print os.getcwd()
+for f in filter(lambda x:x.endswith('.ic'),os.listdir('.')):
+	try:
+		retText = subprocess.check_output('java -cp ../bin1:/usr/share/java/cup.jar Main %s -L../libic.sig 2>&1'%f,shell=True)
+	except subprocess.CalledProcessError,e:
+		retText = e.output
+	try:
+		if open(f[:-2]+'tokens','r').read() == retText.replace('\n','\r\n'):
+			print "Success on %s" % f
+		else:
+			print "Failed on %s" % f
+			open(f[:-2]+'result','w').write(retText.replace('\n','\r\n'))
+	except:
+		open(f[:-2]+'result','w').write(retText.replace('\n','\r\n'))
+	
+
